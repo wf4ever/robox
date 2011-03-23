a5ea8a2c040eed7bfc8484c556797d35188278f0
Jits <dev@jits.co.uk>
2011-03-23 17:01:14 +0000

    WFE-48 - Initial "getting started" section. Not fully functional just yet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,12 @@ module ApplicationHelper
     return link_to(content_for_button, link, options.delete_if{|o| [ :size, :color ].include? o})
   end
   
+  def axialis_icon_path(name, options={})
+    options.reverse_merge!({ 
+      :color => :red,
+      :size => "16x16" 
+    })
+    return "Axialis-Round-Web2-#{options[:color].to_s.capitalize}-Png/Png/#{options[:size]}/#{name}.png"
+  end
+  
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,23 @@
 module HomeHelper
+  
+  def getting_started_status_icon(status)
+    class_name = "status_icon"
+    size = "24x24"
+    image_name = status.to_s.capitalize
+    alt_text = status.to_s.humanize.capitalize
+    color = :green
+    
+    case status
+    when :done
+      image_name = "Ok"
+    when :not_done
+      image_name = "Cancel"
+      color = :red
+    end
+    
+    return image_tag axialis_icon_path(image_name, :color => color, :size => size), 
+      :class => class_name, 
+      :alt => alt_text
+  end
+  
 end
